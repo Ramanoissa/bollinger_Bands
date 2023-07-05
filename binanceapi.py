ec19b58bc75eeff4a7594bc7cc3904d0561812f8
Ramanoissa <127023946+Ramanoissa@users.noreply.github.com>
2023-07-05 15:14:13 +0200

    Update binanceapi.py

--- binanceapi.py
+++ binanceapi.py
@@ -2,8 +2,8 @@ import binance.client
 from binance.client import Client
 
 
-pkey= "ILpQmtDCL4jJwN6PFuHEK1BRbtEpuwBXAnIEI4bjFVO3wsIpPoNk7xGJnOxN1XX9"
+pkey= "****************************************************************"
 
-skey= "oUeyAvqjbV31poCmGdIJuAh4UrwDBmOBMDkkUPJJhxulyIUwQi71Y20KKGEJKdOa"
+skey= "****************************************************************"
 
 client= Client(api_key=pkey, api_secret=skey)
