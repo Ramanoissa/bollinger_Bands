ce3e934773776a34e6b0f09304df74b834335707
Ramanoissa <127023946+Ramanoissa@users.noreply.github.com>
2023-07-05 15:25:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from  calculate_percentage_difference import calculate_percentage_difference
 for symbol in symbols:
     # Get data from API
     klines = []
-    get_prices
+    get_prices()
     df['bb_lowerband_value'] = df.apply(lambda row: row['close'] if row.name is None else row.iloc[4], axis=1)
     df['bb_upperband_value'] = df.apply(lambda row: row['close'] if row.name is None else row.iloc[3], axis=1)
-    calculate_percentage_difference
+    percentage_diff = calculate_percentage_difference(df, symbol)  # Calculate percentage difference
+    print("Bollinger Bands for", symbol)
+    print("Percentage Difference for", symbol, ":", percentage_diff)
 
-print("Bollinger Bands for " + str(symbol))
