ed1dc403289073402ba925c9f29734fafe36c56b
Ramanoissa <127023946+Ramanoissa@users.noreply.github.com>
2023-07-05 19:17:06 +0200

    Update calculate_percentage_difference.py

--- calculate_percentage_difference.py
+++ calculate_percentage_difference.py
@@ -1,6 +1,5 @@
-from symbols import symbols 
+from symbols import symbols
 from bollinger import df
-
 
 def calculate_percentage_difference(df, symbol):
     symbol_data = df[df['symbol'] == symbol]  # Filter data for the given symbol
@@ -11,6 +10,7 @@ def calculate_percentage_difference(df, symbol):
     percentage_diff = (upper - lower) / lower * 100
 
     return percentage_diff
+
 # Assuming you have the DataFrame df with the required columns (including 'symbol', 'upper', and 'lower')
 # Calculate overall percentage difference for all symbols
 percentage_difference = 0
@@ -19,8 +19,6 @@ for symbol in df['symbol'].unique():
     percentage_difference += percentage_diff
 
 print("Overall Percentage Difference:", percentage_difference)
-
-
 
 # Calculate percentage difference for a specific symbol
 percentage_diff = calculate_percentage_difference(df, symbol)
